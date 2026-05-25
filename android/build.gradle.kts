0449908e48350f12b7c0e83c18cad1307b0db53e
Daniel371-lab <jplabscreator@gmail.com>
2026-05-25 17:33:57 +0000

    primer commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -11,9 +18,14 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
